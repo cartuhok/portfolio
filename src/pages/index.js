4896d3dfbe7fd7f1aecc60cea61b4d529e1bd0c2
Carter Rink <carter.m.rink@gmail.com>
2018-09-22 21:14:07 -0400

    d

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Now go build something greatd.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
